0e9a8248349ce9934252f2e17ab81fc80692ca53
leejihoo <ajtwlsdl33@gmail.com>
2024-07-10 01:21:12 +0900

    [기능 추가] StageManager 구현
    스테이지 이동, 스테이지 클리어 여부 확인, 스테이지 설정 기능 추가

--- Assets/Scripts/Stage/StageManager.cs
+++ Assets/Scripts/Stage/StageManager.cs
@@ -8,4 +8,50 @@ public class StageManager : MonoBehaviour
     public int currentStage;
     public Action ClearStage;
     public DynamicGridLayoutGroup grid;
+    public const int lastStage = 3;
+
+    private void Awake() {
+        GameManager.Instance.OnSelectSameCards += RemoveCardPair;
+    }
+
+    private void OnDestroy() {
+        GameManager.Instance.OnSelectSameCards -= RemoveCardPair;
+    }
+
+    public void MoveNextStage(){
+        currentStage += 1;
+        var stageSO = stageSOArray[currentStage-1];
+        GameManager.Instance.SetGameInfo(stageSO.stagePoint,stageSO.stageTimeLimit);
+        SetStage(stageSO);
+    }
+
+    public bool IsExistNextStage(){
+        if(currentStage == lastStage){
+            return false;
+        }
+        return true;
+    }
+    public void RemoveCardPair(){
+        remainingCardPair -= 1;
+
+        if(!IsExistRemainingCardPair()){
+            if(IsExistNextStage()){
+                ClearStage?.Invoke();
+            }
+            else{
+                GameManager.Instance.OnGameClear?.Invoke();
+            }
+        }
+    }
+    public bool IsExistRemainingCardPair(){
+        if(remainingCardPair > 0){
+            return true;
+        }
+
+        return false;
+    }
+    public void SetStage(StageSO stageSO){
+        grid.SetCardSize(stageSO.cardWidth, stageSO.cardHeight);
+        grid.SetStageSize(stageSO.row);
+    }
 }
