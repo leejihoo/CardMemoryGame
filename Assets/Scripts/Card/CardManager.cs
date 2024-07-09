b19abfc497b4214824b284e529effcf76422563f
leejihoo <ajtwlsdl33@gmail.com>
2024-07-10 01:16:43 +0900

    [기능 추가] CardManager 구현
    카드 뒤집기, 랜덤 카드 덱 생성, 카드 셔플, 카드 비활성화, 중복 카드 확인 기능 구현

--- Assets/Scripts/Card/CardManager.cs
+++ Assets/Scripts/Card/CardManager.cs
@@ -1,27 +1,63 @@
 
+using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
-    public Card[] cardDeck;
+    public List<Card> cardDeck;
 
-    public void FlipCard(Card card){
-
+    public void FlipCardsToBackImage(List<Card> cards){
+        foreach(var card in cards){
+            card.isFliped = false;
+            card.cardImage.sprite = card.cardSO.backImage;
+        }
     }
 
-    public Card[] CreateRandomCardDeck(){
-        return null;
+    // 카드를 랜덤 생성할 때 OnClicked에 붙인다.
+    public void FlipCardToFrontImage(Card card){
+        card.isFliped = true;
+        card.cardImage.sprite = card.cardSO.frontImage;
     }
 
-    public void SuffleCardDeck(){
+    public List<Card> CreateRandomCardDeck(int cardPairCount){
+        List<Card> randomCards = new List<Card>();
 
+        while(randomCards.Count < cardPairCount){
+            var randomNum = Random.Range(0,51);
+            var randomCard = cardDeck[randomNum];
+            
+            if(IsExistDuplicateCard(randomCard.cardSO.cardId,randomCards)){
+                continue;
+            }
+
+            // 쌍으로 넣는다.
+            for(int i = 0; i<2; i++){
+                randomCards.Add(randomCard);
+            }
+        }
+
+        SuffleCardDeck(randomCards);
+
+        return randomCards;
+    }
+
+    public void SuffleCardDeck(List<Card> cards){
+        // Unity Documentation Random 내용 사용
+        for (int i = 0; i < cards.Count; i++) {
+            var temp = cards[i];
+            var randomIndex = Random.Range(0, cards.Count);
+            cards[i] = cards[randomIndex];
+            cards[randomIndex] = temp;
+        }
     }
 
     public void UneableCard(Card card){
-
+        card.enabled = false;
     }
 
-    public bool IsExistDuplicateCard(string cardId){
-        return false;
+    public bool IsExistDuplicateCard(string cardId, List<Card> randomCards){
+        var result = randomCards.Exists(x => x.cardSO.cardId == cardId);
+        return result;
     }
 }
