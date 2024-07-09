59eaad00944eb3acf9396d466d1eacfdefa462bf
leejihoo <ajtwlsdl33@gmail.com>
2024-07-10 01:10:09 +0900

    [기능 추가] GamaManager 구현
    게임 상태, 점수, 제한 시간, 선택된 카드를 제어하는 클래스

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,31 +1,178 @@
 using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : Singleton<GameManager>
 {
     public int pointSum;
     public int stagePoint;
     public float time;
+    public float currentTime;
     public bool isExistSeletedCard;
-    public Card selectedCardModel;
+    public bool isGameEnd;
+    public Card selectedCard;
     public Action OnTimeOut;
     public Action OnSelectSameCards;
-    public Action OnSelectDifferentCards;
+    public Action<List<Card>> OnSelectDifferentCards;
     public Action OnGameStart;
+    public Action OnGameClear;
+    public Action OnResetGame;
+
+    public TMP_Text timeText;
+    public TMP_Text pointText;
+    public Button startButton;
+    public Button backButton;
+
+    public override void Awake() {
+        base.Awake();
+        OnTimeOut += GameOver;
+        OnSelectSameCards += GetPoint;
+        OnGameClear += GameOver;
+        
+        OnResetGame += HideBackButton;
+        OnResetGame += ExposeStartButton;
+        OnResetGame += HideText;
+        OnResetGame += ResetGameInfo;
+        
+        OnGameStart += ExposeBackButton;
+        OnGameStart += HideStartButton;
+        OnGameStart += ExposeText;
+    }
+
+    private void Update() {
+        if(!isGameEnd){
+            CountDown();
+        }
+    }
+
+    private void OnDestroy() {
+        OnTimeOut -= GameOver;
+        OnSelectSameCards -= GetPoint;
+        OnGameClear -= GameOver;
+        OnResetGame -= HideBackButton;
+        OnResetGame -= ExposeStartButton;
+        OnResetGame -= HideText;
+        OnResetGame -= ResetGameInfo;
+
+        OnGameStart -= ExposeBackButton;
+        OnGameStart -= HideStartButton;
+        OnGameStart -= ExposeText;
+    }
+
+    public void UpdateTimeText(){
+        timeText.text = "Time: " + Mathf.Ceil(currentTime).ToString() + " sec";
+    }
 
     public void GetPoint(){
-
+        pointSum += stagePoint;
+        UpdatePointText();
     }
+
+    public void UpdatePointText(){
+        pointText.text = "Point: " + pointSum;
+    }
+
     public void CountDown(){
+        currentTime -= Time.deltaTime;
+        if(IsTimeOver(currentTime)){
+            currentTime = 0;
+            OnTimeOut?.Invoke();
+        }
 
+        UpdateTimeText();
     }
-    public bool IsSameCard(){
+
+    public bool IsSameCard(Card card){
+        if(selectedCard.cardSO.cardId == card.cardSO.cardId){
+            return true;
+        }
         return false;
     }
     public void GetCardInfo(Card card){
-
+        if(isExistSeletedCard){
+            if(IsSameCard(card)){
+                OnSelectSameCards?.Invoke();
+            }
+            else{
+                List<Card> cards = new List<Card>
+                {
+                    selectedCard,
+                    card
+                };
+                OnSelectDifferentCards?.Invoke(cards);
+            }
+            selectedCard = null;
+            isExistSeletedCard = false;
+            
+            return;
+        }
+        
+        selectedCard = card;
+        isExistSeletedCard = true;
     }
-    public void SetGameInfo(int stagePoint, float time){
 
+    public void SetGameInfo(int stagePoint, float time){
+        this.time = time;
+        this.stagePoint = stagePoint;
+    }
+
+    public bool IsTimeOver(float time){
+        if(time > 0){
+            return false;
+        }
+
+        return true;
+    }
+
+    public void GameOver(){
+        isGameEnd = true;
+    }
+
+    public void StartGame(){
+        OnGameStart?.Invoke();
+    }
+
+    // 뒤로가기 버튼 & 다시하기 버튼을 눌렀을 때
+    public void ResetGame(){
+        OnResetGame?.Invoke();
+    }
+
+    public void HideText(){
+        timeText.enabled = false;
+        pointText.enabled = false;
+    }
+
+    public void ExposeText(){
+        timeText.enabled = true;
+        pointText.enabled = true;
+    }
+
+    public void HideStartButton(){
+        startButton.enabled = false;
+    }
+
+    public void ExposeStartButton(){
+        startButton.enabled = true;
+    }
+
+    public void HideBackButton(){
+        backButton.enabled = false;
+    }
+
+    public void ExposeBackButton(){
+        backButton.enabled = true;
+    }
+
+    public void ResetGameInfo(){
+        pointSum = 0;
+        stagePoint = 0;
+        time = 0;
+        currentTime = 0;
+        isExistSeletedCard = false;
+        isGameEnd = false;
+        selectedCard = null;
     }
 }
 
