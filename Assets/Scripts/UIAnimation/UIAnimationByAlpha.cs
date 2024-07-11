7b1efefa9242f4e09bbde20be6c4504cdc1c2218
leejihoo <ajtwlsdl33@gmail.com>
2024-07-11 17:05:26 +0900

    [로직 변경] 타겟 오브젝트 알파 값 변경 코드 추가

--- Assets/Scripts/UIAnimation/UIAnimationByAlpha.cs
+++ Assets/Scripts/UIAnimation/UIAnimationByAlpha.cs
@@ -5,6 +5,7 @@ public class UIAnimationByAlpha : UIAnimation
 {
     public override void ExecuteAnimation(Transform target, float animationDuration, Ease ease)
     {
+        target.GetComponent<CanvasGroup>().alpha = 0f;
         target.GetComponent<CanvasGroup>().DOFade(1f, animationDuration).SetEase(ease);
     }
 }
