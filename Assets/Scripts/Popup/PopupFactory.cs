13e59d4d63ad2f5dfb23d562d40bd89387ae98d2
leejihoo <ajtwlsdl33@gmail.com>
2024-07-11 17:00:23 +0900

    [애니메이션 추가] 팝업 창에 애니메이션 추가

--- Assets/Scripts/Popup/PopupFactory.cs
+++ Assets/Scripts/Popup/PopupFactory.cs
@@ -16,16 +16,19 @@ public class PopupFactory : MonoBehaviour
 
         if(popupType == PopupType.GAMECLEAR){
             var popup = Instantiate(gameClearPopup, parent);
+            UIAnimationManager.Instance.ExecuteAnimation(popup.transform,AnimaitonType.Scale);
             ListenResetButtonEvent(popup);
             return popup;
         }
         else if(popupType == PopupType.STAGECLEAR){
             var popup = Instantiate(clearStagePopup, parent);
+            UIAnimationManager.Instance.ExecuteAnimation(popup.transform,AnimaitonType.Scale);
             ListenMoveStageButtonEvent(popup);
             return popup;
         }
         else if(popupType == PopupType.TIMEOUT){
             var popup = Instantiate(timeOutPopup, parent);
+            UIAnimationManager.Instance.ExecuteAnimation(popup.transform,AnimaitonType.Alpha);
             ListenResetButtonEvent(popup);
             return popup;
         }
